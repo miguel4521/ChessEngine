d34a75937a9e0faecd5bf62519e3cce5db62c66e
Miguel Klemm Silva <miguelks_4521@outlook.com>
2023-02-19 20:38:57 +0000

    Changed the array with pieces to type Piece and added the first pieces Also added some helper functions for the position, get occupied squares and get empty squares

--- Position.cs
+++ Position.cs
@@ -3,23 +3,26 @@
 public struct Position
 {
     // An array of bitboards, one for each piece type and color
-    public Bitboard[] bitboards;
+    public Piece[] pieces;
 
     // The constructor that initializes the bitboards to the initial position
     public Position()
     {
-        bitboards = new Bitboard[12];
-        bitboards[0] = new Bitboard(0b0000000000000000000000000000000000000000000000001111111100000000); // white pawns
-        bitboards[1] = new Bitboard(0b0000000011111111000000000000000000000000000000000000000000000000); // black pawns
-        bitboards[2] = new Bitboard(0b0000000000000000000000000000000000000000000000000000000010000001); // white rooks
-        bitboards[3] = new Bitboard(0b1000000100000000000000000000000000000000000000000000000000000000); // black rooks
-        bitboards[4] = new Bitboard(0b0000000000000000000000000000000000000000000000000000000001000010); // white knights
-        bitboards[5] = new Bitboard(0b0100001000000000000000000000000000000000000000000000000000000000); // black knights
-        bitboards[6] = new Bitboard(0b0000000000000000000000000000000000000000000000000000000000100100); // white bishops
-        bitboards[7] = new Bitboard(0b0010010000000000000000000000000000000000000000000000000000000000); // black bishops
-        bitboards[8] = new Bitboard(0b0000000000000000000000000000000000000000000000000000000000001000); // white queen
-        bitboards[9] = new Bitboard(0b0000100000000000000000000000000000000000000000000000000000000000); // black queen
-        bitboards[10] = new Bitboard(0b0000000000000000000000000000000000000000000000000000000000010000); // white king
-        bitboards[11] = new Bitboard(0b0001000000000000000000000000000000000000000000000000000000000000); // black king
+        pieces = new Piece[2];
+        pieces[0] = new Pawn( true); // white pawns
+        pieces[1] = new Pawn(false); // black pawns
+    }
+
+    public Bitboard getOccupiedSquares()
+    {
+        Bitboard occupied = new Bitboard();
+        foreach (Piece piece in pieces)
+            occupied |= piece;
+        return occupied;
+    }
+
+    public Bitboard GetEmptySquares()
+    {
+        return getOccupiedSquares().GetEmptySquares();
     }
 }
