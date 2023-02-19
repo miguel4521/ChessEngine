4b8ff46e5ea0b3603fece0750616fa7886c22275
Miguel Klemm Silva <miguelks_4521@outlook.com>
2023-02-19 20:13:31 +0000

    Added move struct

---
+++ Move.cs
@@ -0,0 +1,13 @@
+﻿namespace ChessEngine;
+
+public struct Move
+{
+    public int From { get; set;}  
+    public int To { get; set;}  
+
+    public Move(int from,int to )  
+    {  
+        From=from ;  
+        To=to ;  
+    }
+}
