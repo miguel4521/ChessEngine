7ae0b7b8d2d0776c85d0210bf9c92b9e73393d91
miguel4521 <109853127+miguel4521@users.noreply.github.com>
2023-03-17 10:53:43 +0000

    Added MaskPawnAttacks function

--- Bitboards/Pieces/Pawn.cs
+++ Bitboards/Pieces/Pawn.cs
@@ -2,14 +2,46 @@
 
 public class Pawn : Piece
 {
+    private readonly Bitboard _notAFile = 0xFEFEFEFEFEFEFEFEUL;
+    private readonly Bitboard _notHFile = 0x7F7F7F7F7F7F7F7FUL;
     public Pawn(bool isWhite)
     {
         IsWhite = isWhite;
         bits = isWhite ? 0x000000000000FF00UL : 0x00FF000000000000UL;
     }
+    
+    private Bitboard MaskPawnAttacks(int square)
+    {
+        // result attacks bitboard
+        Bitboard attacks = 0UL;
+
+        // piece bitboard
+        Bitboard bitboard = 0UL;
+
+        bitboard[square] = true;
+
+        // white pawns
+        if (IsWhite)
+        {
+            // generate pawn attacks
+            if (((bitboard >> 7) & _notAFile) != 0) attacks |= bitboard >> 7;
+            if (((bitboard >> 9) & _notHFile) != 0) attacks |= bitboard >> 9;
+        }
+        // black pawns
+        else
+        {
+            // generate pawn attacks
+            if (((bitboard << 7) & _notHFile) != 0) attacks |= bitboard << 7;
+            if (((bitboard << 9) & _notAFile) != 0) attacks |= bitboard << 9;    
+        }
+    
+        // return attack map
+        return attacks;
+    }
 
     public override List<Move> GenerateMoves(Position position)
     {
+
         return null;
     }
 }
